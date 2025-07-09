ea18028b0df7edd30a51ed51abe4befd5bf82e56
Rafael Francisco <rafael.palma.franc@gmail.com>
2025-07-09 15:28:34 -0300

    Documento upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { MenuClienteComponent } from './pages/menu-cliente/menu-cliente.componen
 import { AuthGuard } from './guards/auth.guard';
 import { TesteComponent } from './pages/teste/teste';
 import { AgendamentoComponent } from './pages/agendamento/agendamento';
+import { DocumentoUploadPageComponent } from './pages/documentos-upload/documento-upload-page.component';
 import { LandingComponent } from './pages/landing-page/landing.component';
 
 export const routes: Routes = [
@@ -30,5 +31,5 @@ export const routes: Routes = [
 
   {path: 'agendamento', component: AgendamentoComponent},
 
-  
+  { path: 'documentos/upload', component: DocumentoUploadPageComponent },
 ];
